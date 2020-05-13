59be73ba7749c8fb8c3cc9ebdb39a83596665b7b
etienne-trooptravel <etienne@trooptravel.com>
2020-05-14 01:21:30 +0200

    Merged MSAL code, Fixed misc eslint errors

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,86 @@
 <template>
   <div id="app">
     <div id="nav">
+      <img src="./assets/logo.png">
+      <h1>{{ msg }}</h1>
+      <button @click="login" type="button" v-if="!user">Login with Microsoft</button>
+      <button @click="callAPI" type="button" v-if="user">
+        Call Graph's /me API
+      </button>
+      <button @click="logout" type="button" v-if="user">
+        Logout
+      </button>
+      <h3 v-if="user">Hello {{ user.name }}</h3>
+      <pre v-if="userInfo">{{ JSON.stringify(userInfo, null, 4) }}</pre>
+      <p v-if="loginFailed">Login unsuccessful</p>
+      <p v-if="apiCallFailed">Graph API call unsuccessful</p>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import AuthService from './services/auth.service';
+import GraphService from './services/graph.service';
+export default {
+  name: 'app',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js + MSAL.js App',
+      user: null,
+      userInfo: null,
+      apiCallFailed: false,
+      loginFailed: false
+    }
+  },
+  created() {
+    this.authService = new AuthService();
+    this.graphService = new GraphService();
+  },
+  methods: {
+    callAPI() {
+      this.apiCallFailed = false;
+      this.authService.getToken().then(
+        token => {
+          this.graphService.getUserInfo(token).then(
+            data => {
+              this.userInfo = data;
+            },
+            error => {
+              console.error(error);
+              this.apiCallFailed = true;
+            }
+          );
+        },
+        error => {
+          console.error(error);
+          this.apiCallFailed = true;
+        }
+      );
+    },
+    logout() {
+      this.authService.logout();
+    },
+    login() {
+      this.loginFailed = false;
+      this.authService.login().then(
+        user => {
+          if (user) {
+            this.user = user;
+          } else {
+            this.loginFailed = true;
+          }
+        },
+        () => {
+          this.loginFailed = true;
+        }
+      );
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,6 +89,26 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button {
+  margin: 15px;
 }
 
 #nav {
---
+++ src/services/auth.service.js
@@ -0,0 +1,65 @@
+import * as Msal from 'msal'
+
+export default class AuthService {
+  constructor () {
+    const PROD_REDIRECT_URI = 'https://vue-msal-sample.firebaseapp.com/'
+    let redirectUri = window.location.origin
+    if (window.location.hostname !== '127.0.0.1') {
+      redirectUri = PROD_REDIRECT_URI
+    }
+    this.applicationConfig = {
+      clientID: '956ee261-b0f8-4964-8140-677bb6d1c58d',
+      graphScopes: ['user.read']
+    }
+    this.app = new Msal.UserAgentApplication(
+      this.applicationConfig.clientID,
+      '',
+      () => {
+        // callback for login redirect
+      },
+      {
+        redirectUri
+      }
+    )
+  }
+
+  login () {
+    return this.app.loginPopup(this.applicationConfig.graphScopes).then(
+      idToken => {
+        const user = this.app.getUser()
+        if (user) {
+          return user
+        } else {
+          return null
+        }
+      },
+      () => {
+        return null
+      }
+    )
+  };
+
+  logout () {
+    this.app.logout()
+  };
+
+  getToken () {
+    return this.app.acquireTokenSilent(this.applicationConfig.graphScopes).then(
+      accessToken => {
+        return accessToken
+      },
+      error => {
+        return this.app
+          .acquireTokenPopup(this.applicationConfig.graphScopes)
+          .then(
+            accessToken => {
+              return accessToken
+            },
+            err => {
+              console.error(err)
+            }
+          )
+      }
+    )
+  };
+}
